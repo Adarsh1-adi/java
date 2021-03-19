6f4ef5264ce4423f7f5aa1281f9b5e906a25977f
Adarsh Sharma <62586318+Adarsh1-adi@users.noreply.github.com>
2021-03-19 17:24:04 +0530

    Add files via upload

---
+++ optionpane.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import javax.swing.*;  
+public class optionpane 
+{  
+JFrame f;  
+optionpane()
+{  
+    f=new JFrame();  
+    JOptionPane.showMessageDialog(f,"ERROR 404. FILE CANT BE OPEN");  
+}  
+public static void main(String[] args)
+ {  
+    new optionpane();  
+}  
+}
+    
+
